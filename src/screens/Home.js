46a91a2fe25f48c96f732f26113af52bb4fb1da3
Techno Kid <technokid0101@gmail.com>
2020-01-23 17:49:46 +0530

    Logout Btn added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import {StyleSheet, YellowBox} from 'react-native'
-import { Text, Container, Content, Left, Button, Icon, Title, Body, Right, Header ,Drawer} from 'native-base'
+import {StyleSheet, YellowBox, PermissionsAndroid, Alert} from 'react-native'
+import { Text, Container, Content, Left, Button, Icon, Title, Body, Right, Header ,Drawer, Toast} from 'native-base'
 import SideBar from './SideBar'
+import { Actions } from 'react-native-router-flux'
+import AsyncStorage from '@react-native-community/async-storage'
 
 
 export default class Home extends React.Component{
-
+    constructor(props){
+        super(props)
+        this.state = {
+            showToast: false
+        }
+    }
     closeDrawer = () => { this._drawer._root.close() }
     openDrawer  = () => { this._drawer._root.open()  } 
+
     render(){
         return (
-            <>
+            <Container>
+
             <Drawer 
                 ref={ (ref) => { this._drawer = ref }} 
                 content={ <SideBar navigator={this._navigator} />} 
@@ -27,14 +36,37 @@ export default class Home extends React.Component{
                     <Body>
                         <Title>SVS</Title>
                     </Body>
-                    <Right style={{maxWidth:60}}>
-                        <Button transparent>
-                            <Icon name='thumbs-up' />
+                    <Right style={{maxWidth:60}} >
+                        <Button transparent >
+                            <Icon name='options'/>
                         </Button>
                     </Right>
                     <Right style={{maxWidth:60}}>
-                        <Button transparent>
-                            <Icon name='warning' />
+                        <Button transparent 
+                                onPress={async()=>{
+                                    console.log("Pressed")
+                                    
+                                    Alert.alert(
+                                        'Attention',
+                                        'Are you sure you want to log out ?',
+                                        [
+                                            {
+                                                text: 'No',
+                                                style: 'cancel'
+                                            },
+                                            {
+                                                text: 'Yes',
+                                                onPress: async()=>{
+                                                    await AsyncStorage.setItem('isUserLoggedIn', 'false')
+                                                    Actions.auth()
+                                                },
+                                                style: 'ok'
+                                            }
+                                        ]
+                                        )
+                                 }}
+                        >
+                            <Icon name='log-out' />
                         </Button>
                     </Right>
                 </Header>
@@ -43,7 +75,7 @@ export default class Home extends React.Component{
                 </Content>
             </Container>
             </Drawer> 
-            </>
+            </Container>
         )
     }
 }
